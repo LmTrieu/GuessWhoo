3958a92dcca62ec836b2a48d4ff04feb4a881a85
LmTrieu <lmtrieu250902@gmail.com>
2024-10-23 23:06:20 +0700

    Added name generation
    + Using name-generator.gd (From https://github.com/xsellier/godot-name-generator) to generate names for peeps.
    + Added Exit button

--- Scene/Game/game.gd
+++ Scene/Game/game.gd
@@ -4,18 +4,23 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var i: int = 0;
-	var names = ["John","Jack","Jill","Bill","Cum","Dump"];
 
-	while i <= 5:
+	const name_gen = preload("res://HelperScript/name-generator.gd");
+
+	while i <= 17:
+		var names = name_gen.generate(4,10);
 		var peep = peep_scene.instantiate();
-		var picked_name = names.pick_random();
-
-		peep.get_node("VBoxContainer/Label").text = picked_name;
-		names.erase(picked_name);
+		
+		peep.get_node("VBoxContainer/Label").text = names;
 		
 		get_tree().get_root().get_node("Game/GridContainer").add_child(peep);
 		i += 1;
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_texture_button_pressed():
+	get_tree().get_current_scene().queue_free();
+	get_tree().change_scene_to_file("res://Scene/MainMenu/main_menu.tscn");
